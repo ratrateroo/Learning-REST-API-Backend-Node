2b6d667e145fb7d7fc62de177f24b39b4bf56cbf
Mark Leo <ratrateroo@gmail.com>
2019-11-27 14:49:39 +0800

    initial commit

---
+++ client codes on other domain/script.js
@@ -0,0 +1,24 @@
+const getButton = document.getElementById('get');
+const postButton = document.getElementById('post');
+
+getButton.addEventListener('click', () => {
+  fetch('http://localhost:8080/feed/posts')
+  .then(res => res.json())
+  .then(resData => console.log(resData))
+  .catch(err => console.log(err));
+});
+postButton.addEventListener('click', () => {
+  fetch('http://localhost:8080/feed/post', {
+    method: 'POST',
+    body: JSON.stringify({
+      title: 'A Codepen Post',
+      content: 'Created via Codepen'
+    }),
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  })
+  .then(res => res.json())
+  .then(resData => console.log(resData))
+  .catch(err => console.log(err));
+});
