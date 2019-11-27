39eb261c57e0f9004ab7bfdd58ddf6793789516f
Mark Leo <ratrateroo@gmail.com>
2019-11-28 02:55:46 +0800

    updated posts

--- routes/feed.js
+++ routes/feed.js
@@ -19,9 +19,22 @@ router.post('/post', [
     .trim()
     .isLength({
         min: 5
-    }),
+    })
 ], feedController.createPost);
 
 router.get('/post/:postId', feedController.getPost);
+
+router.put('/post/:postId', [
+    body('title')
+    .trim()
+    .isLength({
+        min: 5
+    }),
+    body('content')
+    .trim()
+    .isLength({
+        min: 5
+    })
+], feedController.updatePost);
 
 module.exports = router;
