a3708b8adba1cd5106f2b1b234738106495cbe73
Mark Leo <ratrateroo@gmail.com>
2019-11-27 20:27:39 +0800

    fixed to respond a single post

--- controllers/feed.js
+++ controllers/feed.js
@@ -3,18 +3,16 @@ const { validationResult } = require('express-validator/check');
 const Post = require('../models/post');
 
 exports.getPosts = (req, res, next) => {
-    res.status(200).json({
-        posts: [{
-            _id: '1',
-            title: 'First Post',
-            content: 'This is the first post!',
-            imageUrl: 'images/duck.jpg',
-            creator: {
-                name: 'Mark'
-            },
-            createdAt: new Date()
-        }]
+    Post.find()
+    .then(posts => {
+        res.status(200).json({message: 'Fetched post succesfully.', posts: posts})
+    })
+    .catch(err => {
+        if(!err.statusCode) {
+            err.statusCode = 500
+        }
     });
+
 };
 
 exports.createPost = (req, res, next) => {
@@ -53,4 +51,23 @@ exports.createPost = (req, res, next) => {
     
 
     console.log(title, content);
+};
+
+exports.getPost = (req, res, next) => {
+    const postId = req.params.postId;
+    Post.findById(postId)
+        .then(post => {
+            if(!post) {
+                const error = new Error('Could not find post.');
+                error.statusCode = 404;
+                throw error;
+            }
+            res.status(200).json({message: 'Post fetched.', post: post});
+        })
+        .catch(err => {
+            if (!err.statusCode) {
+                err.statusCode = 500;
+            }
+            next(err);
+        });
 };
--- routes/feed.js
+++ routes/feed.js
@@ -22,4 +22,6 @@ router.post('/post', [
     }),
 ], feedController.createPost);
 
+router.get('/post/:postId', feedController.getPost);
+
 module.exports = router;
