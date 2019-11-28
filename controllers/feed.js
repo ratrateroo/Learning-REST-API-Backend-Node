0d73a160bb7c0376ee13ac5020a20cd0f712df5b
Mark Leo <ratrateroo@gmail.com>
2019-11-29 01:55:26 +0800

    connected posts and users

--- controllers/feed.js
+++ controllers/feed.js
@@ -4,6 +4,7 @@ const path = require('path');
 const { validationResult } = require('express-validator/check');
 
 const Post = require('../models/post');
+const User = require('../models/user');
 
 exports.getPosts = (req, res, next) => {
     const currentPage = req.query.page || 1;
@@ -51,24 +52,34 @@ exports.createPost = (req, res, next) => {
     const imageUrl = req.file.path.replace("\\","/");
     const title = req.body.title;
     const content = req.body.content;
+    let creator;
     // Create post in db
     const post = new Post({
         title: title,
-        content:content,
+        content: content,
         imageUrl: imageUrl,
-        creator: { name: 'Mark '}
+        creator: req.userId
     });
     post
         .save()
         .then(result => {
+            return User.findById(req.userId);
+        })
+        .then(user => {
+            creator = user;
+            user.posts.push(post);
+            return user.save();
+            
+        })
+        .then(result => {
             console.log(result);
             res.status(201).json({
                 message: 'Post created successfully!',
-                post: result
+                post: post,
+                creator: { _id: creator._id, name: creator.name }
             });
         })
         .catch(err => {
-            console.log('Creating Post');
             if (!err.statusCode) {
                 err.statusCode = 500;
             }
