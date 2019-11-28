6e459c82302a0b18623d3da643dd6217bdbc485b
Mark Leo <ratrateroo@gmail.com>
2019-11-28 16:25:21 +0800

    added pagination

--- controllers/feed.js
+++ controllers/feed.js
@@ -6,15 +6,31 @@ const { validationResult } = require('express-validator/check');
 const Post = require('../models/post');
 
 exports.getPosts = (req, res, next) => {
+    const currentPage = req.query.page || 1;
+    const perPage = 2;
+    let totalItems;
     Post.find()
-    .then(posts => {
-        res.status(200).json({message: 'Fetched post successfully.', posts: posts})
-    })
-    .catch(err => {
-        if(!err.statusCode) {
-            err.statusCode = 500
-        }
-    });
+        .countDocuments()
+        .then(count => {
+            totalItems = count;
+            return Post.find()
+                .skip((currentPage - 1) * perPage)
+                .limit(perPage);
+        })
+        .then(posts => {
+            res.status(200).json({
+                message: 'Fetched post successfully.',
+                posts: posts,
+                totalItems: totalItems
+            });
+        })        
+        .catch(err => {
+            if (!err.statusCode) {
+                err.statusCode = 500
+            }
+            next(err);
+        });
+
 
 };
 
