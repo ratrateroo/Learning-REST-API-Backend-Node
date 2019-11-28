a30ce8ece805beba4d6f70d74854e50de0b20b47
Mark Leo <ratrateroo@gmail.com>
2019-11-29 02:03:24 +0800

    added authorization check for edit and delete post

--- controllers/feed.js
+++ controllers/feed.js
@@ -137,6 +137,11 @@ exports.updatePost = (req, res, next) => {
             error.statusCode = 404;
             throw error;
         }
+        if (post.creator.toString() !== req.userId) {
+            const error = new Error('Not authorized!');
+            error.statusCode = 403;
+            throw error;
+        }
         if (imageUrl !== post.imageUrl) {
             clearImage(post.imageUrl);
         }
@@ -166,6 +171,11 @@ exports.deletePost = (req, res, next) => {
         if(!post) {
             const error = new Error('Could not find post.');
             error.statusCode = 404;
+            throw error;
+        }
+        if (post.creator.toString() !== req.userId) {
+            const error = new Error('Not authorized!');
+            error.statusCode = 403;
             throw error;
         }
         // Check logged in user
