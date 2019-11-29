036ff06da3e7f065032d8d83769932f847ecdeb4
Mark Leo <ratrateroo@gmail.com>
2019-11-29 21:05:02 +0800

    shared io instance across files

--- app.js
+++ app.js
@@ -61,7 +61,7 @@ app.use((error, req, res, next) => {
 mongoose.connect(MONGODB_URI)
     .then(result => {
         const server = app.listen(8080);
-        const io = require('socket.io')(server);
+        const io = require('./socket').init(server);
         io.on('connection', socket => {
             console.log('Client connected');
         });
